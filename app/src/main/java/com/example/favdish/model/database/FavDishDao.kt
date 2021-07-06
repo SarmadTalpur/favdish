9daf4bebe232240ecb97329abe416204c9e2c03b
Sarmad <sarmadtalpur60@gmail.com>
2021-07-06 19:21:18 +0500

    Initial Commit.

---
+++ app/src/main/java/com/example/favdish/model/database/FavDishDao.kt
@@ -0,0 +1,17 @@
+package com.example.favdish.model.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.favdish.model.entities.FavDish
+import java.util.concurrent.Flow
+
+@Dao
+interface FavDishDao {
+
+    @Insert
+    suspend fun addFavDishDetails (favDish: FavDish)
+
+    @Query("SELECT * FROM FAV_DISHES_TABLE ORDER BY ID")
+    fun getAllDishesList(): kotlinx.coroutines.flow.Flow<List<FavDish>>
+}
