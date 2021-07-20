12f294263ea9782f929c0dc720d37f6c005285cc
Sarmad <sarmadtalpur60@gmail.com>
2021-07-20 22:01:22 +0500

    favorite dish to dish details

--- app/src/main/java/com/example/favdish/view/fragments/FavoriteDishesFragment.kt
+++ app/src/main/java/com/example/favdish/view/fragments/FavoriteDishesFragment.kt
@@ -9,9 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.favdish.application.FavDishApplication
 import com.example.favdish.databinding.FragmentFavoriteDishesBinding
+import com.example.favdish.model.entities.FavDish
+import com.example.favdish.view.activities.MainActivity
 import com.example.favdish.view.adapters.FavDishAdapter
 import com.example.favdish.viewmodel.DashboardViewModel
 import com.example.favdish.viewmodel.FavDishViewModel
@@ -40,11 +43,6 @@ class FavoriteDishesFragment : Fragment() {
         return mBinding!!.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        mBinding = null
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mFavDishViewModel.favoriteDishes.observe(viewLifecycleOwner) {
@@ -63,6 +61,30 @@ class FavoriteDishesFragment : Fragment() {
                     mBinding!!.tvNoFavoriteDishesAvailable.visibility = View.VISIBLE
                 }
             }
+        }
+    }
+
+    fun dishDetails(favDish: FavDish) {
+
+        findNavController().navigate(FavoriteDishesFragmentDirections.actionFavoriteDishesToDishDetails(
+            favDish
+        ))
+
+        if (requireActivity() is MainActivity){
+            (activity as MainActivity?)!!.hideBottomNavigationView()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (requireActivity() is MainActivity){
+            (activity as MainActivity?)!!.showBottomNavigationView()
         }
     }
 }
