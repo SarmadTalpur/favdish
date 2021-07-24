0afd0c289f8d12948c9dbd4b254a064242ea1cd2
Sarmad <sarmadtalpur60@gmail.com>
2021-07-24 16:17:57 +0500

    edit dish implemented.

--- app/src/main/java/com/example/favdish/view/adapters/FavDishAdapter.kt
+++ app/src/main/java/com/example/favdish/view/adapters/FavDishAdapter.kt
@@ -1,12 +1,19 @@
 package com.example.favdish.view.adapters
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.favdish.R
 import com.example.favdish.databinding.ItemDishLayoutBinding
 import com.example.favdish.model.entities.FavDish
+import com.example.favdish.utils.Constants
+import com.example.favdish.view.activities.AddUpdateDishActivity
 import com.example.favdish.view.fragments.AllDishesFragment
 import com.example.favdish.view.fragments.FavoriteDishesFragment
 
@@ -17,6 +24,7 @@ class FavDishAdapter(private val fragment: Fragment): RecyclerView.Adapter<FavDi
     class ViewHolder(view: ItemDishLayoutBinding) : RecyclerView.ViewHolder(view.root) {
         val ivDishImage = view.ivDishImage
         val tvTitle = view.tvDishTitle
+        val ibMore = view.ibMore
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -38,6 +46,32 @@ class FavDishAdapter(private val fragment: Fragment): RecyclerView.Adapter<FavDi
             if (fragment is FavoriteDishesFragment) {
                 fragment.dishDetails(dish)
             }
+        }
+
+        holder.ibMore.setOnClickListener {
+            val popup = PopupMenu(fragment.context, holder.ibMore)
+            popup.menuInflater.inflate(R.menu.menu_adapter, popup.menu)
+
+            popup.setOnMenuItemClickListener {
+                if (it.itemId == R.id.action_edit_dish) {
+                    val intent = Intent(fragment.requireActivity(), AddUpdateDishActivity::class.java)
+                    intent.putExtra(Constants.EXTRA_DISH_DETAILS, dish)
+                    fragment.requireActivity().startActivity(intent)
+
+
+
+                } else if (it.itemId == R.id.action_delete_dish) {
+                    Log.i("You clicked on", "Delete option of ${dish.title}")
+                }
+                true
+            }
+            popup.show()
+        }
+
+        if (fragment is AllDishesFragment) {
+            holder.ibMore.visibility = View.VISIBLE
+        } else if (fragment is FavoriteDishesFragment) {
+            holder.ibMore.visibility = View.GONE
         }
     }
 
